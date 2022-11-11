7c7b158b7709076617de131bf869a408be5715bb
Casca0 <lucashborzani@gmail.com>
2022-11-10 21:27:26 -0300

    Required express

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
+const express = require('express');
 const { Player } = require('discord-player');
 
 process.env.DISCORD_TOKEN;
