a417175e1331c0a7ba67439b4a798a4a1e029cc3
Casca0 <lucashborzani@gmail.com>
2023-06-22 03:26:38 -0300

    Reversed changes

--- src/commands/teste.js
+++ src/commands/teste.js
@@ -1,6 +1,4 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const { useMasterPlayer } = require('discord-player/dist');
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('teste')
@@ -12,18 +10,6 @@ module.exports = {
 		)
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction) {
-		const player = useMasterPlayer();
-
-		const query = interaction.options.getString('query');
-
-		const searchResult = await player.search(query, {
-			requestedBy: interaction.user,
-		}).catch((err) => {
-			console.log(err);
-		});
-
-		console.log(searchResult);
-
 		return interaction.reply('Teste');
 	},
 };
