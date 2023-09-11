d09a9856f663959e3ce8999447b260e3c4b8bc22
Casca0 <lucashborzani@gmail.com>
2023-09-11 02:52:12 -0300

    fixed bug in debug event

--- src/events/debug.js
+++ src/events/debug.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'debug',
 	music: true,
 	execute(queue, message) {
-		console.log(`[DEBUG] ${queue.guild.id} ${message}`);
+		console.log(`[DEBUG] ${queue?.guild.id} ${message}`);
 	},
 };
